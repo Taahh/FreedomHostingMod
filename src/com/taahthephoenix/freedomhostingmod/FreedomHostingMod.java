83bd4cceb23df70e961e2b4003763ffc57e5efb5
ThePheonixBlader <thepheonixblader@gmail.com>
2016-02-13 18:39:59 -0800

    Update FreedomHostingMod.java

--- src/com/taahthephoenix/freedomhostingmod/FreedomHostingMod.java
+++ src/com/taahthephoenix/freedomhostingmod/FreedomHostingMod.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.taahthephoenix.freedomhostingmod;
 
-/**
- *
- * @author anis_sayed
- */
-public class FreedomHostingMod {
+import com.taahthephoenix.freedomhostingmod.commands.Command_fhm;
+import net.pravian.aero.command.handler.AeroCommandHandler;
+import net.pravian.aero.command.handler.SimpleCommandHandler;
+import net.pravian.aero.config.YamlConfig;
+import net.pravian.aero.plugin.AeroPlugin;
+
+public class FreedomHostingMod extends AeroPlugin {
+    public static FreedomHostingMod plugin;
+    public static YamlConfig config;
+    public static AeroCommandHandler handler;
     
+    @Override
+    public void load(){
+        this.plugin = this;
+    }
+    
+    @Override
+    public void enable(){
+        getLogger().info("FreedomHosting Mod enabled! Made by AvalancheYT and TaahThePhoenix");
+        
+        handler = new SimpleCommandHandler(plugin);
+        handler.setCommandClassPrefix("Command_");
+        handler.loadFrom(Command_fhm.class.getPackage());
+        handler.registerAll();
+    }
+    
+    @Override
+    public void disable(){
+        getLogger().info("FreedomHosting Mod disabled! Made by AvalancheYT and TaahThePhoenix");
+    }
 }
